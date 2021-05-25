a864cc555042dbfb4e9110b6517c25f8364fbf93
Arash Zaheri <arashzaheri@gmail.com>
2021-05-24 19:50:19 -0500

    final

--- calc.js
+++ calc.js
@@ -42,8 +42,7 @@ reader.question("What would you like to calculate?", function(input){
     }
     if (mathSymbol === "mod"){
         console.log(num1 % num2)
-    
+	}
 	// This line closes the connection to the command line interface.
 	reader.close()
-
 });
