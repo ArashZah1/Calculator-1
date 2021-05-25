f3dee06a3ad395cc456a9e03afc9ec6e4d6cb7d9
Arash Zaheri <arashzaheri@gmail.com>
2021-05-24 19:48:09 -0500

    Final Further Study

--- calc.js
+++ calc.js
@@ -31,7 +31,18 @@ reader.question("What would you like to calculate?", function(input){
 	if (mathSymbol === "*") {
 		console.log(num1 * num2)
 	}
-	
+	if (mathSymbol === "sqr"){
+        console.log(Math.pow(num1,2))
+    }
+    if (mathSymbol === "cube"){
+        console.log(Math.pow(num1,3))
+    }
+    if (mathSymbol === "pow"){
+        console.log(Math.pow(num1,num2))
+    }
+    if (mathSymbol === "mod"){
+        console.log(num1 % num2)
+    
 	// This line closes the connection to the command line interface.
 	reader.close()
 
